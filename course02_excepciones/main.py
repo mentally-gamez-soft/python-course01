37e94a6e94499aadcdeb1d507014342b84c90b79
christian zoror <christian.zoror@akka.eu>
2022-11-10 18:36:44 +0100

    how to raise exceptions

--- course02_excepciones/main.py
+++ course02_excepciones/main.py
@@ -1,5 +1,6 @@
 import traceback
 import sys
+import datetime
 
 """
 try:
@@ -23,6 +24,7 @@ print('on est apres la division')
 """
 
 
+"""
 try:
     n1 = float(input('Introduis un nombre: '))
     n2 = float(input('Introduis un nombre: '))
@@ -37,3 +39,22 @@ finally:
     print('Je suis le finally, je m execute si o si')
 
 print('---  fin du traitement ----')
+
+"""
+
+
+# invoquer une exception
+num = -32
+try:
+    if num <= 0:
+        raise ValueError('Erreur le prix est negatif')
+except ValueError as e:
+    print('le prix ne peut pas etre negatif =>', e)
+
+try:
+    num = int(input('introduire une valeur:'))
+except ValueError as e:
+    # print(type(e).__name__)
+    with open('errors.log','a') as f:
+        now = datetime.datetime.now()
+        f.write('\n' + str(now) + ' - [02_exceptions -> main.py]' + str(e) + ' -> ' + str(type(e).__name__))
