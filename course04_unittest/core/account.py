7655f8d4010cac3314c940204617666f04284d35
christian zoror <christian.zoror@akka.eu>
2022-11-21 11:54:49 +0100

    add a docstring to the class Account

--- course04_unittest/core/account.py
+++ course04_unittest/core/account.py
@@ -1,4 +1,13 @@
 class Account():
+    """ Classe Account: represente un compte bancaire
+
+    Un account possede une propriété:
+    - balance: le solde du compte en banque
+
+    Raises:
+        Exception: 
+          levée si le client essaye de retirer plus que son solde
+    """    
     balance = 0 # variable de classe
 
     def add_amount(self, amount):
